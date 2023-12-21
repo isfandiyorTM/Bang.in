30b7abab1cb00e866ddbca634062f8771dc864ec
Isfandiyor Madminov <isfandiyormadaminov12@gmail.com>
2023-12-21 09:58:12 +0500

    changes

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ menu_btn.addEventListener("click", () => {
     menu.classList.toggle("show");
     register_btn.classList.toggle("show");
 });
+
+// nav
+const nav = document.querySelector("header nav");
+const nav_container = document.querySelector("header nav .container");
+const navigation = document.querySelector("header nav .container .navigation");
+
+window.addEventListener("scroll", () => {
+    menu.classList.remove("show");
+
+    if (window.scrollY > 40) {
+        nav.classList.add("sticky");
+        nav_container.classList.add("sticky");
+        navigation.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+        nav_container.classList.remove("sticky");
+        navigation.classList.remove("sticky");
+    }
+});
